95fea687f83344607dd9bf1ed05f81333f9b43c9
Alex Sidorov <promoabys@gmail.com>
2019-09-30 23:26:19 +0300

    init: Add solve of task

--- src/index.js
+++ src/index.js
@@ -9,6 +9,31 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      const TIMETOMASTER = 800;
+      const TIMETOBASIC = 500;
+
+      let fullTime;
+      let countOfWeeks = 0;
+
+      console.log("knowsProgramming = " + knowsProgramming);
+      if(knowsProgramming) fullTime = TIMETOMASTER;
+      else fullTime = TIMETOMASTER+TIMETOBASIC;
+    
+      // console.log("Config = " + config);
+
+      console.log("FOCUS = " + focus);
+
+/*       for (const [key, value] of Object.entries(config)) {
+        console.log(key + " = " + value);
+         countOfAffairs = countOfAffairs + value;
+      } */
+      
+      console.log("FullTime = " + fullTime);
+      console.log("Config[focus] = " + config[focus]);
+      countOfWeeks = Math.ceil(fullTime/config[focus]);
+      console.log(countOfWeeks);
+      // countOfWeeks = 80;
+
+      return countOfWeeks;
   };
   
